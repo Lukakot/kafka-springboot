f435446471657a71f84c647e4f55a51b2c9df139
kot_lukasz <lukaszkot95@gmail.com>
2024-07-16 13:53:05 +0200

    nullpointer for StreamsBuilderFactoryBean fix

--- order-app/src/main/java/com/kafkastreams/service/OrderStoreService.java
+++ order-app/src/main/java/com/kafkastreams/service/OrderStoreService.java
@@ -5,6 +5,7 @@ import org.apache.kafka.streams.state.QueryableStoreType;
 import org.apache.kafka.streams.state.QueryableStoreTypes;
 import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 import org.apache.kafka.streams.state.ReadOnlyWindowStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,11 @@ import org.springframework.stereotype.Service;
 public class OrderStoreService {
 
     StreamsBuilderFactoryBean streamsBuilderFactoryBean;
+
+    @Autowired
+    public OrderStoreService(StreamsBuilderFactoryBean streamsBuilderFactoryBean){
+        this.streamsBuilderFactoryBean = streamsBuilderFactoryBean;
+    }
 
     public ReadOnlyKeyValueStore<String, Long> ordersCountStore(String storeName) {
 
